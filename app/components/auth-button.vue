11bf043528011c78899a9aa1d934f0478c3670fc
rm-reins <rm-reins@icloud.com>
2025-08-15 23:41:22 +0200

    feat: update authentication UI with dropdown for signed-in users and add sign-out page

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -5,7 +5,25 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+  <details v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <summary class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </summary>
+    <ul class="menu dropdown-content bg-base-200 border-1 border-white rounded-box z-1 w-52 p-2 mt-3 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </details>
+
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sing In With Github
     <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
     <Icon v-else name="tabler:brand-github" size="24" />

0361e7f6c722ece15bd728105b76da113c15746b
rm-reins <rm_reins@icloud.com>
2025-08-19 10:31:00 +0200

    refactor: restructure database connection and fix import path

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { createAuthMiddleware } from "better-auth/plugins";
 
-import db from "./db/indext";
+import db from "./db";
 import env from "./env";
 
 export const auth = betterAuth({

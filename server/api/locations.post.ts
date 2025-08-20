ccb6ce89b8cafa3c88e42809648196786cb2a537
rm-reins <rm_reins@icloud.com>
2025-08-20 05:22:10 +0200

    feat: add location insertion API and type definitions for user context

---
+++ server/api/locations.post.ts
@@ -0,0 +1,43 @@
+import db from "~/lib/db";
+import { InsertLocation, location } from "~/lib/db/schemas";
+
+export default defineEventHandler(async (event) => {
+  if (!event.context.user) {
+    return sendError(event, createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    }));
+  }
+
+  const result = await readValidatedBody(event, InsertLocation.safeParse);
+
+  if (!result.success) {
+    const statusMessage = result
+      .error
+      .issues
+      .map(issue => `${issue.path.join("")}: ${issue.message}`)
+      .join("; ");
+
+    const data = result
+      .error
+      .issues
+      .reduce((errors, issue) => {
+        errors[issue.path.join("")] = issue.message;
+        return errors;
+      }, {} as Record<string, string>);
+
+    return sendError(event, createError({
+      statusCode: 422,
+      statusMessage,
+      data,
+    }));
+  }
+
+  await db.insert(location).values({
+    ...result.data,
+    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
+    userId: event.context.user.id,
+  });
+
+  return result.data;
+});

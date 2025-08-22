50328d4d7ea285e931424fe3d315e953ec0c133c
rm-reins <rm_reins@icloud.com>
2025-08-22 05:42:11 +0200

    feat: add form field component and remove dashboard page

---
+++ app/components/app/form-field.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  error?: string;
+  label: string;
+  name: string;
+  type: string;
+  as?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="props.as"
+      :type="props.type"
+      :name="props.name"
+      :disabled="disabled"
+      class="w-full"
+      :class="{ 'input-error': props.error,
+                'input': type === 'text' || 'number',
+                'textarea': type === 'textarea',
+      }"
+    />
+    <p v-if="props.error" class="label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
